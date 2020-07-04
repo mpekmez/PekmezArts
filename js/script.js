8dea33d9d52d6afc7b501f25caa992230915f56a
Marijana Pekmezovic <pekmezovicm@gmail.com>
2020-07-04 22:36:54 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    var p = document.getElementById('buttonMore');
+
+    p.addEventListener('click', function(event) {
+        p.innerHTML = 'You clicked it!';
+        p.style.color = 'purple';
+        p.style.fontSize = '60px';
+    });
+
+    var s = document.getElementById('imgClick');
+
+    s.addEventListener('click', function (event) {
+        location.href = 'shop.html'
+    });
+
+
+
+
+
+});
+
+
